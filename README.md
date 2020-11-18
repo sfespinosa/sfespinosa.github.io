682b7faad342e81cbb48bd246dd478b0537ede88
sfespinosa <scottjames.espinosa@gmail.com>
2020-11-18 15:33:54 -0800

    added README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Scott Espinosa
+You've reached the repository for my portfolio website! The site can be accessed at <a>scottespinosa.com</a>.
+
+For more about me, visit these sites:<br/>
+LinkedIn - https://www.linkedin.com/in/scottespinosa/<br/>
+Medium - https://scottespinosa.medium.com/<br/>
+
+Need to reach me? Email scottjames.espinosa@gmail.com.
+
+Thanks for visiting!
--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
-let blogArray = []
+// let blogArray = []
 
-const getMediumFeed = () => {
-    fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40scottespinosa')
-        .then(res => res.json())
-        .then(json => {
-            blogArray = [json.items[0].description, json.items[1].description, json.items[2]]
-            appendBlogs()
-        })
-}
+// const getMediumFeed = () => {
+//     fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40scottespinosa')
+//         .then(res => res.json())
+//         .then(json => {
+//             blogArray = [json.items[0].description, json.items[1].description, json.items[2]]
+//             appendBlogs()
+//         })
+// }
 
-const appendBlogs = () => {
-    let blogDiv = document.querySelector('div.blogs')
-    let blogContent = blogArray[0] + blogArray[1]
-    debugger
-    blogDiv.innerHTML = blogContent
-}
+// const appendBlogs = () => {
+//     let blogDiv = document.querySelector('div.blogs')
+//     let blogContent = blogArray[0] + blogArray[1]
+//     blogDiv.innerHTML = blogContent
+// }
 
-getMediumFeed()
+// getMediumFeed()
 
 $(".js-video-button").modalVideo({
     youtube:{
